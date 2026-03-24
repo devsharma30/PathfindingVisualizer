84f38ca207c43721cb2538ca7e10e33046ffa472
Dev Sharma <devsharma30@gmail.com>
2026-03-24 20:43:04 +0530

    Added SFML window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Pathfinding Visualizer Started" << std::endl;
+    std::cout << "Program started\n";
+
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "Pathfinding Visualizer");
+
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
