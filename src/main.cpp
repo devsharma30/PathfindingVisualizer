bc4f7aeb7080b075b2c9fde0fb4e9988a2c52b2a
Dev Sharma <devsharma30@gmail.com>
2026-03-26 23:57:27 +0530

    Added grid rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
 
 int main()
 {
-    std::cout << "Program started\n";
+    const int ROWS = 20;
+    const int COLS = 20;
+    const int CELL_SIZE = 30;
 
-    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "Pathfinding Visualizer");
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(COLS * CELL_SIZE, ROWS * CELL_SIZE)), "Pathfinding Visualizer");
 
     while (window.isOpen())
     {
@@ -16,6 +17,21 @@ int main()
         }
 
         window.clear();
+
+        // DRAW GRID
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                sf::RectangleShape cell(sf::Vector2f(CELL_SIZE - 1, CELL_SIZE - 1));
+
+                cell.setPosition(sf::Vector2f(j * CELL_SIZE, i * CELL_SIZE));
+                cell.setFillColor(sf::Color::White);
+
+                window.draw(cell);
+            }
+        }
+
         window.display();
     }
 
